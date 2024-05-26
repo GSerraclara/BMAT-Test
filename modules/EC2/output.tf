9a7af12364f4f082e6cc6a366b032ea5d9bc14e4
Guillermo Serraclara <guillermo_sv96@hotmail.com>
2024-05-27 00:14:54 +0200

    Output EC2 Fix

--- modules/EC2/output.tf
+++ modules/EC2/output.tf
@@ -1,11 +1,7 @@
-output "instance_ids" {
-  value = aws_autoscaling_group.web_server_asg.id
-}
-
 output "web_server_asg_name" {
   value = aws_autoscaling_group.web_server_asg.name
 }
 
-output "autoscaling_group_instance" {
+output "web_server_asg_instance" {
   value = aws_autoscaling_group.web_server_asg.id
 }
