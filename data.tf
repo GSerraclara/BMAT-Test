3806cf6ba59e9451ea96ee2eeb173a7501b370cd
Guillermo Serraclara <guillermo_sv96@hotmail.com>
2024-05-27 00:18:07 +0200

    local removed

--- data.tf
+++ data.tf
@@ -8,8 +8,3 @@ data "aws_instances" "web_server_instances" {
     values = [data.aws_autoscaling_group.web_server_asg.id]
   }
 }
-
-#data "local_file" "instance_ids" {
-#  depends_on = [null_resource.get_instance_ids]
-#  filename   = "${path.module}/instance_ids.txt"
-#}
